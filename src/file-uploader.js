7f700ba5148c1441d584634497126832ae38b778
Dinesh T <dineshselvantdm@gmail.com>
2019-10-06 12:35:10 +0530

    Added state machine

--- src/file-uploader.js
+++ src/file-uploader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DragAndDrop from "./drag-and-drop";
 
 const config = {
@@ -7,13 +7,39 @@ const config = {
   filesLimit: 1
 };
 
+const FILE_UPLOADER_STATE = {
+  INIT: "INIT",
+  PROCESSING: "PROCESSING",
+  SUCCESS: "SUCCESS",
+  FAILURE: "FAILURE"
+};
+
 const FileUploader = () => {
-  const processDrop = files => {};
+  const [loaderState, setLoaderState] = useState(FILE_UPLOADER_STATE.INIT);
+  const processDrop = files => {
+    //Simulate async request for file upload
+    setTimeout(() => {
+      setLoaderState(FILE_UPLOADER_STATE.PROCESSING);
+    }, 1000);
+    setTimeout(() => {
+      setLoaderState(FILE_UPLOADER_STATE.SUCCESS);
+    }, 3000);
+  };
   return (
-    <DragAndDrop processDrop={processDrop} config={config}>
-      <div>Drag and drop files here</div>
-      <div>State machine based on file upload</div>
-    </DragAndDrop>
+    <>
+      {loaderState === FILE_UPLOADER_STATE.INIT && (
+        <DragAndDrop processDrop={processDrop} config={config}>
+          <div>Drag and drop files here</div>
+          <div>State machine based on file upload</div>
+        </DragAndDrop>
+      )}
+      {loaderState === FILE_UPLOADER_STATE.PROCESSING && (
+        <div className="drag-container">Processing...</div>
+      )}
+      {loaderState === FILE_UPLOADER_STATE.SUCCESS && (
+        <div className="drag-container">File Upload done!</div>
+      )}
+    </>
   );
 };
 
